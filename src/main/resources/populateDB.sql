2a8aa64ba4704dc3c1e142bdeb80f1f87a75debf
TimSelivan <timselivanson@gmail.com>
2023-03-11 17:06:30 +0300

    Connect, create and populate databases

---
+++ src/main/resources/populateDB.sql
@@ -0,0 +1,9 @@
+insert into customer(name, document_number, phone_number, email)
+values ('Tom', 123, 987, 'tom@yahoo.com'),
+       ('Ann', 456, 654, 'ann@yahoo.com'),
+       ('Bob', 789, 321, 'bob@yahoo.com');
+
+insert into bicycle(brand_name, model, serial_number)
+values ('merida', 'road bike', 123),
+       ('bulls', 'mountain bike', 456),
+       ('giant', 'electric bike', 789);
