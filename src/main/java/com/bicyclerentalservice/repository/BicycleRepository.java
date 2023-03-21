dcbfa7e46a980a34ada2810bb25dcd9487e452ce
TimSelivan <timselivanson@gmail.com>
2023-03-21 12:38:42 +0300

    Add repository template classes

---
+++ src/main/java/com/bicyclerentalservice/repository/BicycleRepository.java
@@ -0,0 +1,16 @@
+package com.bicyclerentalservice.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BicycleRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BicycleRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+}
---
+++ src/main/java/com/bicyclerentalservice/repository/CustomerRepository.java
@@ -0,0 +1,16 @@
+package com.bicyclerentalservice.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CustomerRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public CustomerRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+}
