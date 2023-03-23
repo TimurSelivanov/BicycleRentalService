429e818e3e4f012c1381cde575f8e990221bf9c3
TimSelivan <timselivanson@gmail.com>
2023-03-23 11:27:59 +0300

    Implement methods in BicyclesController

--- src/main/java/com/bicyclerentalservice/controller/BicyclesController.java
+++ src/main/java/com/bicyclerentalservice/controller/BicyclesController.java
@@ -1,15 +1,17 @@
 package com.bicyclerentalservice.controller;
 
+import com.bicyclerentalservice.model.Bicycle;
 import com.bicyclerentalservice.model.Customer;
 import com.bicyclerentalservice.repository.BicyclesRepository;
 import com.bicyclerentalservice.repository.CustomersRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/bicycles")
@@ -32,9 +34,65 @@ public class BicyclesController {
 
     @GetMapping("/{id}")
     public String getById(@PathVariable("id") int id, Model model, @ModelAttribute("customer") Customer customer) {
+        //add bicycle to model
         model.addAttribute("bicycle", bicycleRepository.getById(id));
+        //get lessee
+        Optional<Customer> lessee = bicycleRepository.getCustomerByBicycleId(id);
 
-        //TODO add method for assigning bicycles to customers first
+        if (lessee.isPresent()) {
+            model.addAttribute("lessee", lessee.get());
+        } else {
+            model.addAttribute("customers", customerRepository.getAll());
+        }
         return "bicycles/details";
     }
+
+    @GetMapping("/new")
+    public String newBicycle(@ModelAttribute("bicycle") Bicycle bicycle) {
+        return "bicycles/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("bicycle") @Valid Bicycle bicycle, BindingResult bindingResult) {
+        if(bindingResult.hasErrors())
+            return "bicycles/new";
+
+        bicycleRepository.save(bicycle);
+        return "redirect:/bicycles";
+    }
+
+    @GetMapping("/{id}/update")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("bicycle", bicycleRepository.getById(id));
+        return "bicycles/update";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("bicycle") @Valid Bicycle bicycle, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if(bindingResult.hasErrors())
+            return "bicycles/update";
+
+        bicycleRepository.update(id, bicycle);
+        return "redirect:/bicycles";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        bicycleRepository.delete(id);
+        return "redirect:/bicycles";
+    }
+
+    @PatchMapping("/{id}/return")
+    public String returnBicycle(@PathVariable("id") int id) {
+        bicycleRepository.returnBicycle(id);
+        return "redirect:/bicycles/" + id;
+    }
+
+    @PatchMapping("/{id}/rent")
+    public String rentBicycle(@PathVariable("id") int id, @ModelAttribute("customer") Customer customer) {
+        bicycleRepository.rentBicycle(id, customer);
+        return "redirect:/bicycles/" + id;
+    }
+
 }
