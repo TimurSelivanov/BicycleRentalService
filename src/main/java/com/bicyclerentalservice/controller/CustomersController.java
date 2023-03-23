d5464bba875141ef25b61ca1581d147ce85901b8
TimSelivan <timselivanson@gmail.com>
2023-03-23 11:57:47 +0300

    Implement methods in CustomersController

--- src/main/java/com/bicyclerentalservice/controller/CustomersController.java
+++ src/main/java/com/bicyclerentalservice/controller/CustomersController.java
@@ -1,12 +1,14 @@
 package com.bicyclerentalservice.controller;
 
+import com.bicyclerentalservice.model.Customer;
 import com.bicyclerentalservice.repository.BicyclesRepository;
 import com.bicyclerentalservice.repository.CustomersRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/customers")
@@ -25,5 +27,49 @@ public class CustomersController {
     public String getAll(Model model) {
         model.addAttribute("customers", customerRepository.getAll());
         return "customers/customers";
+    }
+
+    @GetMapping("/{id}")
+    public String getById(@PathVariable("id") int id, Model model) {
+        model.addAttribute("customer", customerRepository.getById(id));
+        model.addAttribute("bicycles", customerRepository.getBicyclesByCustomerId(id));
+        return "customers/details";
+    }
+
+    @GetMapping("/new")
+    public String newCustomer(@ModelAttribute("customer")Customer customer) {
+        return "customers/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("customer") @Valid Customer customer, BindingResult bindingResult) {
+        //TODO customer unique validation
+        if(bindingResult.hasErrors())
+            return "customers/new";
+
+        customerRepository.save(customer);
+        return "redirect:/customers";
+    }
+
+    @GetMapping("/{id}/update")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("customer", customerRepository.getById(id));
+        return "customers/update";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("customer") @Valid Customer customer, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if(bindingResult.hasErrors())
+            return "customers/update";
+
+        customerRepository.update(id, customer);
+        return "redirect:/customers";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        customerRepository.delete(id);
+        return "redirect:/customers";
     }
 }
